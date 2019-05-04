084096a4be6a6025bf8866117984b53c2660edc0
sahil97 <sahil.sharma071997@gmail.com>
2019-05-04 21:41:09 +0530

    added multipart form upload

--- app.py
+++ app.py
@@ -47,25 +47,52 @@ def home():
 
 @app.route('/api/v0.1/inference',methods = ['POST'])
 def inference():
-    content = request.json
-    imagePath = content["path"]
-    test = pd.read_csv('test.csv')
-    test_image = []
-    for i in tqdm(range(test.shape[0])):
-        # img = image.load_img(test['id'][i].astype('str')+'.png', target_size=(28,28,1), grayscale=True)
-        img = image.load_img(imagePath, target_size=(28,28,1), grayscale=True)
-        img = image.img_to_array(img)
-        img = img/255
-        test_image.append(img)
-    test = np.array(test_image)
-    model = load_kmodel()
-    prediction = model.predict_classes([test])
-    # CLEARING SESSION FOR MODEL TO LOAD AGAIN
-    K.clear_session()
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+            return jsonify({
+                "error":"No file"
+            })
+            # return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('No selected file')
+            return jsonify({
+                "error":"No selected file"
+            })
+            # return redirect(request.url)
+        if file:
+            print(file.filename)
+            file.save(file.filename)
+            imagePath = file.filename
+            test = pd.read_csv('test.csv')
+            test_image = []
+            for i in tqdm(range(test.shape[0])):
+                # img = image.load_img(test['id'][i].astype('str')+'.png', target_size=(28,28,1), grayscale=True)
+                img = image.load_img(imagePath, target_size=(28,28,1), grayscale=True)
+                img = image.img_to_array(img)
+                img = img/255
+                test_image.append(img)
+            test = np.array(test_image)
+            model = load_kmodel()
+            prediction = model.predict_classes([test])
+            # CLEARING SESSION FOR MODEL TO LOAD AGAIN
+            K.clear_session()
 
-    # RETURNING PREDICTION
+            # RETURNING PREDICTION
+            return jsonify({
+                "prediction":str(prediction[0])
+            })
+    # if request.method == 'POST'
+    # content = request.json
+
+
+    #
     return jsonify({
-        "prediction":str(prediction[0])
+        "prediction":"test 1"
     })
 
 
--- index.js
+++ index.js
@@ -3,8 +3,21 @@ $(document).ready(()=>{
     $('#sendData').click(function(){
 
     // Storing the URL
-    var imgData = $('#urlText').val();
+    // var imgData = $('#urlText').val();
+    var fileSelect = document.getElementById('file-select');
+    var files = fileSelect.files;
+    console.log(files);
+    // console.log(files);
 
+    var form = new FormData();
+    for (const file of files) {
+      form.append('file', file);
+    }
+    // form.append("file", "780.png");
+    console.log(form);
+    // Loop through each of the selected files.
+
+    // console.log(formData);
     //Sending AJAX CALL
     $.ajax({
       "async": true,
@@ -12,16 +25,19 @@ $(document).ready(()=>{
       "url": "http://localhost:5000/api/v0.1/inference",
       "method": "POST",
       "headers": {
-        "content-type": "application/json",
-        "cache-control": "no-cache"
+        "cache-control": "no-cache",
+        "postman-token": "91a0d5be-9674-4788-6682-93a0ae049d3d"
       },
       "processData": false,
-      "data": JSON.stringify(
-        {"path":imgData.toString()}
-      ),
+      "contentType": false,
+      "mimeType": "multipart/form-data",
+      "data": form,
       "success": function(data) {
         // DISPLAYING THE RESULT
-        document.getElementById('result').innerHTML = data['prediction'];
+        data = JSON.parse(data);
+        console.log(data);
+        console.log(data["prediction"])
+        document.getElementById('result').innerHTML = data["prediction"];
       }
       });
     });
